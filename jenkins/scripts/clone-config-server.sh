d2e67b96862e7a85c57edd4be0941ee5eec6807a
melek homrani <melek.homrani@nttdata.com>
2025-07-06 16:01:20 +0100

    jenkins config

--- jenkins/scripts/clone-config-server.sh
+++ jenkins/scripts/clone-config-server.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "🔄 Adding ssh key"
-
-ssh-add ~/.ssh/github_personal/id_rsa
-
 echo "🔄 Cloning public config-server repo..."
 
-git clone git@github.com:melekhomrani/config-server.git config-server
+git clone https://github.com/melekhomrani/config-server.git config-server
 
 echo "✅ Config-server cloned inside $(pwd)/config-server"
