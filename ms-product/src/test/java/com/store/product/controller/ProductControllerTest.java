e7f0422cd997c05cdaf3dd0c71bd352e4610e052
melek homrani <73800650+melekhomrani@users.noreply.github.com>
2025-07-07 12:22:15 +0100

    fix ms-product tests

--- ms-product/src/test/java/com/store/product/controller/ProductControllerTest.java
+++ ms-product/src/test/java/com/store/product/controller/ProductControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.List;
 
 @WebMvcTest(ProductController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class ProductControllerTest {
 
     @Autowired
--- ms-product/src/test/java/com/store/product/repository/ProductRepositoryTest.java
+++ ms-product/src/test/java/com/store/product/repository/ProductRepositoryTest.java
@@ -2,28 +2,16 @@ package com.store.product.repository;
 
 import com.store.product.model.Product;
 
-import net.bytebuddy.utility.dispatcher.JavaDispatcher.Container;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MongoDBContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
 @DataMongoTest
 class ProductRepositoryTest {
 
-    @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0");
-
-    @DynamicPropertySource
-    static void mongoProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-    }
 
     @Autowired
     private ProductRepository productRepository;
