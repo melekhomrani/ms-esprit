7cbc7eb3112da6394762f3d14e951aa829c1ee8b
melekhomrani <melek.homrani@esprit.tn>
2025-07-07 01:01:54 +0100

    typo fix

--- ms-notification/src/main/java/com/store/notification/email/EmailService.java
+++ ms-notification/src/main/java/com/store/notification/email/EmailService.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
-import com.store.dto.ManufacturerDto;
-
 import java.util.HashMap;
 import java.util.Map;
 
