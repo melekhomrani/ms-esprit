a4d6fc38955bcc4099141e9d95f47c9e66946485
melekhomrani <melek.homrani@esprit.tn>
2025-07-08 15:37:37 +0100

    clean npm cache

--- jenkins/scripts/build_run_authentication.sh
+++ jenkins/scripts/build_run_authentication.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 echo "Building and running authentication NodeJS service..."
+npm cache clean --force
+npm install -g npm
 docker-compose up --build -d authentication-app
 docker-compose logs -f authentication-app
