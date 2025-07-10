656d165a63d3def125e765597a03be5d5f1c38e8
melek homrani <73800650+melekhomrani@users.noreply.github.com>
2025-07-10 16:48:30 +0100

    Microservice (#6)
    * no need
    
    * ms
    
    * ms

--- ms-product/src/main/java/com/store/product/feign/ManufacturerClient.java
+++ ms-product/src/main/java/com/store/product/feign/ManufacturerClient.java
@@ -2,20 +2,16 @@ package com.store.product.feign;
 
 import com.store.dto.ManufacturerDto;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
     name = "ms-manufacturer",
-    fallback = ManufacturerClientFallback.class,
     configuration = FeignConfig.class
 )
 public interface ManufacturerClient {
 
     @GetMapping("/api/manufacturers/{id}")
-    @CircuitBreaker(name = "manufacturerClient")
     ManufacturerDto getManufacturer(@PathVariable("id") Long id);
 }
