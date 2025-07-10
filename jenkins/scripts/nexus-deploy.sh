b2c8f017f6b2afd6655445f420a7080922d2e2f5
melek homrani <73800650+melekhomrani@users.noreply.github.com>
2025-07-10 16:26:17 +0100

    Mission entreprise (#4)
    * Enforce LF endings
    
    * make scripts executable (+x)
    
    * make scripts executable (+x)
    
    * add .dockerignore to prevent build errors
    
    * UPDATE
    
    * logs for auth
    
    * logs for auth
    
    * update jenkinsfile
    
    * clean npm cache
    
    * fix clean npm cache
    
    * fix clean npm cache
    
    * fix start keycloak with imported realm
    
    * rm logs from auth app build
    
    * jenkins pipeline
    
    ---------
    
    Co-authored-by: melek homrani <melek.homrani@nttdata.com>

--- jenkins/scripts/nexus-deploy.sh
+++ jenkins/scripts/nexus-deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 echo "🚀 Deploying artifacts to Nexus..."
-mvn deploy -DaltDeploymentRepository=nexus::default::http://localhost:8081/repository/maven-releases/
+
+mvn deploy -DskipTests
+
 if [ $? -ne 0 ]; then
     echo "❌ Deployment to Nexus failed. Exiting..."
     exit 1
