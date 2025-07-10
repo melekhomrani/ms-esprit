4790000a3c18fba0ca48088b12ed6ac718a044ee
melek homrani <melek.homrani@nttdata.com>
2025-07-10 16:16:36 +0100

    jenkins pipeline

--- jenkins/scripts/start_databases.sh
+++ jenkins/scripts/start_databases.sh
@@ -4,6 +4,3 @@ echo "Starting databases, Zookeeper, Kafka, and Keycloak with realm import..."
 docker-compose up -d keycloakdb
 docker-compose up -d productdb
 docker-compose up -d manufacturerdb
-docker-compose up -d zookeeper
-docker-compose up -d kafka
-docker-compose up -d keycloak
