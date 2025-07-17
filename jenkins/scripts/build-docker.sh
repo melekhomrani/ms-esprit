78e04c55f9627d0388b9f796b9557fa85621ecba
melek homrani <melek.homrani@nttdata.com>
2025-07-17 15:22:01 +0100

    change docker build

--- jenkins/scripts/build-docker.sh
+++ jenkins/scripts/build-docker.sh
@@ -4,9 +4,12 @@ set -e
 
 eval $(minikube docker-env)
 
-services=(authentication config-server eureka-server gateway ms-manufacturer ms-product ms-notification)
+# services=(authentication config-server eureka-server gateway ms-manufacturer ms-product ms-notification)
 
-for service in "${services[@]}"; do
-  echo "Building Docker image for $service"
-  docker build -t $service:latest ./$service
-done
+docker build -t ms-esprit/authentication:latest -f ./authentication/Dockerfile ./authentication
+docker build -t ms-esprit/config-server:latest -f ./config-server/Dockerfile ./config-server
+docker build -t ms-esprit/eureka-server:latest -f ./eureka-server/Dockerfile ./eureka-server
+docker build -t ms-esprit/gateway:latest -f ./gateway/Dockerfile ./gateway
+docker build -t ms-esprit/ms-manufacturer:latest -f ./ms-manufacturer/Dockerfile ./ms-manufacturer
+docker build -t ms-esprit/ms-product:latest -f ./ms-product/Dockerfile ./ms-product
+docker build -t ms-esprit/ms-notification:latest -f ./ms-notification/Dockerfile ./ms-notification
