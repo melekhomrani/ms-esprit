300a3beb57da301b22e2500460d47f620cd4d4a9
melek homrani <melek.homrani@nttdata.com>
2025-07-18 10:50:17 +0100

    fix k8s

--- jenkins/scripts/deploy-k8s.sh
+++ jenkins/scripts/deploy-k8s.sh
@@ -7,14 +7,4 @@ set -x
 export MINIKUBE_HOME=/home/melek
 export KUBECONFIG=/mnt/c/Users/lek/.kube/config
 
-echo "Deploying to Kubernetes..."
-
-minikube image load localhost:5000/authentication-app:latest
-minikube image load localhost:5000/config-server:latest
-minikube image load localhost:5000/eureka-server:latest
-minikube image load localhost:5000/gateway:latest
-minikube image load localhost:5000/ms-manufacturer:latest
-minikube image load localhost:5000/ms-product:latest
-minikube image load localhost:5000/ms-notification:latest
-
 kubectl apply -f k8s/
