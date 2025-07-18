0355cec04140ad07d371a5601c0a9da19832837b
melek homrani <melek.homrani@nttdata.com>
2025-07-18 10:43:06 +0100

    fix k8s images

--- jenkins/scripts/deploy-k8s.sh
+++ jenkins/scripts/deploy-k8s.sh
@@ -6,4 +6,12 @@ export KUBECONFIG=/mnt/c/Users/lek/.kube/config
 
 echo "Deploying to Kubernetes..."
 
+minikube image load localhost:5000/authentication-app:latest
+minikube image load localhost:5000/config-server:latest
+minikube image load localhost:5000/eureka-server:latest
+minikube image load localhost:5000/gateway:latest
+minikube image load localhost:5000/ms-manufacturer:latest
+minikube image load localhost:5000/ms-product:latest
+minikube image load localhost:5000/ms-notification:latest
+
 kubectl apply -f k8s/
