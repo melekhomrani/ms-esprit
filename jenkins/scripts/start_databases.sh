3847fbb73d716efaf65bc94793ef898e60e256c7
melek homrani <melek.homrani@nttdata.com>
2025-07-18 10:36:27 +0100

    fix nexus

--- jenkins/scripts/start_databases.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-echo "Starting databases, Zookeeper, Kafka, and Keycloak with realm import..."
-docker-compose up -d keycloakdb
-docker-compose up -d productdb
-docker-compose up -d manufacturerdb
